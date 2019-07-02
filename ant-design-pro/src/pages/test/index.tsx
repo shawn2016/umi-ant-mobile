0dfb99f8f67f62d539e40cc093315c6691e39c13
shawn2016 <787990963@qq.com>
2019-07-02 15:16:13 +0800

    init

---
+++ ant-design-pro/src/pages/test/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default (): React.ReactNode => (
+  <p style={{ textAlign: 'center' }}>
+    test to{' '}
+    <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
+      use block
+    </a>
+    。
+  </p>
+);
